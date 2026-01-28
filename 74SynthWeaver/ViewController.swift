ea3d5fdb9ed2acd5d145108cef98985ec746fe62
Roman <romazan.26@gmail.com>
2026-01-28 21:03:56 +0300

    Initial Commit

---
+++ 74SynthWeaver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  74SynthWeaver
+//
+//  Created by Роман Главацкий on 28.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
